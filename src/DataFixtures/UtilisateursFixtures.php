419ffdb31f4955143579505765abff91eaeca350
Medhi Gendrot <mehdig35@hotmail.fr>
2025-09-01 13:56:54 +0200

    mise en place des fixtures utilisateurs

---
+++ src/DataFixtures/UtilisateursFixtures.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Campus;
+use App\Entity\Utilisateur;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UtilisateursFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+
+    }
+    public function load(ObjectManager $manager): void
+    {
+       $this->ajouterUtilisateurs($manager);
+    }
+
+    public function ajouterUtilisateurs(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $campus = new Campus();
+        $campus->setNom('Campus Test');
+        $manager->persist($campus);
+
+        $utilisateur = new Utilisateur();
+
+        $utilisateur->setNom($faker->firstName());
+        $utilisateur->setPrenom($faker->lastName());
+        $utilisateur->setEmail('organisateur@mail.fr');
+        $utilisateur->setPassword($this->userPasswordHasher->hashPassword($utilisateur, 'organisateur'));
+        $utilisateur->setRoles(['ROLE_USER']);
+        $utilisateur->setTelephone($faker->phoneNumber());
+        $utilisateur->setEstActif(true);
+        $utilisateur->setCampus($campus);
+        $manager->persist($utilisateur);
+
+        $participant = new Utilisateur();
+
+        $participant->setNom($faker->firstName());
+        $participant->setPrenom($faker->lastName());
+        $participant->setEmail('participant@mail.fr');
+        $participant->setPassword($this->userPasswordHasher->hashPassword($participant, 'participant'));
+        $participant->setRoles(['ROLE_USER']);
+        $participant->setTelephone($faker->phoneNumber());
+        $participant->setEstActif(true);
+        $participant->setCampus($campus);
+        $manager->persist($participant);
+
+        $admin = new Utilisateur();
+
+        $admin->setNom($faker->firstName());
+        $admin->setPrenom($faker->lastName());
+        $admin->setEmail('admin@mail.fr');
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, 'admin'));
+        $admin->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $admin->setTelephone($faker->phoneNumber());
+        $admin->setEstActif(true);
+        $admin->setCampus($campus);
+        $manager->persist($admin);
+
+        $manager->flush();
+
+        
+    }
+}
